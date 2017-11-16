08fd924cf63750cc803e315d8aa7df74da79b424
swetharevanur <swetharevanur@Swethas-MacBook-Pro-2.local>
2017-11-16 01:20:03 -0800

    complete textual preprocessing with stop word removal, punctuation stripping, leetspeak translation, and additional cleaning operations

---
+++ stopwords.py
@@ -0,0 +1,7 @@
+# stopwords.py
+# List of stop words
+# Author: Swetha Revanur and Keanu Spies
+
+ENGL_STOP_WORDS = ['i','a','about','an','are','as','at','be','by','for', 'am', \
+				'from','how','in','is','it','of','on','or','that','the', \
+				'this','to','was','what','when','where','who','will','with','the']
--- util.py
+++ util.py
@@ -1,18 +1,19 @@
 # util.py
-# Helper functions
-# Author: Swetha Revanur
+# Helper functions used in parsing, preprocessing, etc.
+# Author: Swetha Revanur and Keanu Spies
 
-import re
+import string
 
+# removes all punctuation
 def stripPunctuation(s):
-	newS = re.sub(r'[^\w\s]', '', s)
-	re.split(r'\s*', newS)
-	return ''.join(newS)
+	return s.translate(None, string.punctuation)
 
+# removes all alphabetical characters
 def stripAlpha(s):
 	newS = re.sub( '[^\\d]', "", s)	
 	return newS
 
+# removes all HTML tag artifacts
 def stripTags(s):
 	newS = re.sub('\</*\w*\>', '', s)
 	re.split(r'\s*', newS)
