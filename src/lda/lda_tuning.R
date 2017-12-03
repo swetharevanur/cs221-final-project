ff9172b97a1dc74136dabbf1265776de239ab029
Swetha Revanur <swetharevanur@gmail.com>
2017-12-02 20:14:06 -0800

    added in label spreading and semi sup SVM

--- src/lda/lda_tuning.R
+++ src/lda/lda_tuning.R
@@ -23,16 +23,17 @@ library(ldatuning)
 data("AssociatedPress", package="topicmodels")
 full_data <- AssociatedPress
 
-system.time({
-  tunes <- FindTopicsNumber(
-    full_data,
-    topics = c(1:10 * 10, 120, 140, 160, 180, 0:3 * 50 + 200),
-    metrics = c("Griffiths2004", "CaoJuan2009", "Arun2010"),
-    method = "Gibbs",
-    control = list(seed = 77),
-    mc.cores = 4L,
-    verbose = TRUE
-  )
-})
 
-FindTopicsNumber_plot(tunes)
+# system.time({
+#   tunes <- FindTopicsNumber(
+#     full_data,
+#     topics = c(1:10 * 10, 120, 140, 160, 180, 0:3 * 50 + 200),
+#     metrics = c("Griffiths2004", "CaoJuan2009", "Arun2010"),
+#     method = "Gibbs",
+#     control = list(seed = 77),
+#     mc.cores = 4L,
+#     verbose = TRUE
+#   )
+# })
+# 
+# FindTopicsNumber_plot(tunes)
---
+++ src/learning/README.txt
@@ -0,0 +1,11 @@
+Script execution order:
+
+Label Spreading
+1. Run label_spreading.py
+
+Semi-supervised SVM
+1. 
+
+Semi-supervised Quadratic Discriminant Analysis
+1. 
+
