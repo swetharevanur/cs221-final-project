48b897236d53157dbecbe4d1b086704e7a06c5ee
Swetha Revanur <swetharevanur@gmail.com>
2017-12-04 07:09:49 -0800

    final lda features, num_topics=7, nwords=4

--- src/lda/lda.py
+++ src/lda/lda.py
@@ -48,24 +48,20 @@ corpus = [dictionary.doc2bow(text) for text in texts]
 
 # generate LDA model
 # optimal number of topics computed in lda_tuning.R
-opt_num_topics = 10
+opt_num_topics = 7
 ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=opt_num_topics, id2word = dictionary, passes=20, minimum_probability=0.0)
 
-# print ldamodel.print_topics(num_topics=opt_num_topics, num_words=4)
+print ldamodel.print_topics(num_topics=opt_num_topics, num_words=4)
 
 # create feature vector dataframe
 # posts are rows and cols are vector components
 index = df['postID']
 columns = ['phi_' + str(i) for i in range(opt_num_topics)]
 
-# featureDF = featureDF.fillna(0) 
-
 rows = []
 for i in range(len(doc_set)):
 	vals = {'phi_' + str(x): y for x,y in ldamodel[corpus[i]]}
 	rows.append(vals)
-	# print vals
-	# featureDF.iloc[[i]] = vals
 
 featureDF = pd.DataFrame(rows, index=index, columns=columns)
 
